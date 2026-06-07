5f4ea6121305c84f5924bfa5454e2eff046f62b3
hisham <heshamelsauied@gmail.com>
2026-06-07 14:24:09 +0300

    refactor(auth): convert signup api route to server actions

---
+++ app/server-actions/auth/signup.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { apiFetch } from "@/lib/api-client";
+import { cookies } from "next/headers";
+import {
+  ACCESS_TOKEN_OPTIONS,
+  REFRESH_TOKEN_OPTIONS,
+  accessToken,
+  refreshToken,
+} from "@/lib/constant";
+
+export type SignupFormData = {
+  email: string;
+  password: string;
+  data: {
+    name: string;
+    jobTitle?: string;
+  };
+};
+
+export default async function signup(signupData: SignupFormData) {
+  try {
+    const response = await apiFetch({
+      path: "/auth/v1/signup",
+      method: "POST",
+      body: JSON.stringify(signupData),
+    });
+
+    const cookieStore = await cookies();
+    
+    cookieStore.set(accessToken, response.access_token, ACCESS_TOKEN_OPTIONS);
+    cookieStore.set(refreshToken, response.refresh_token, REFRESH_TOKEN_OPTIONS);
+
+    return response.user.user_metadata;
+  } catch (error) { 
+    console.log(error)
+    throw typeof error === "string"
+      ? error
+      : "Failed to create account";
+  }
+}
