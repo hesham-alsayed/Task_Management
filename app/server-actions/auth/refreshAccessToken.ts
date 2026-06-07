6a68f3cd18204edcd495eb9e46ef691fd79b5143
hisham <heshamelsauied@gmail.com>
2026-06-07 14:33:26 +0300

    refactor(auth): convert refresh token api route to server actions

---
+++ app/server-actions/auth/refreshAccessToken.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { apiFetch } from "@/lib/api-client";
+import { accessToken, refreshToken } from "@/lib/constant";
+
+export async function refreshAccessToken() {
+  try {
+    const cookieStore = await cookies();
+    const refresh_token = cookieStore.get("refresh_token")?.value;
+    console.log(refresh_token)
+    if (!refresh_token) {
+      throw "No refresh token";
+    }
+
+    const data = await apiFetch({
+      path: `/auth/v1/token?grant_type=refresh_token`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ refresh_token }),
+    });
+
+    cookieStore.set(accessToken, data.access_token, {
+      httpOnly: true,
+      path: "/",
+      maxAge: 120,
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    console.log(data)
+    return data
+  } catch (error) {
+    throw typeof error === "string" ? error : "Refresh failed";
+  }
+}
