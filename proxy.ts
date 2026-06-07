6602bbaf1e466b7776f0e7b2ed409da60b8f2df0
Hesham Al Sayed <heshamelsauied@gmail.com>
2026-06-07 14:59:57 +0300

    reafactor : Increase access token max age to 3600 seconds
    Updated the access token max age from 120 seconds to 3600 seconds.

--- proxy.ts
+++ proxy.ts
@@ -16,18 +16,14 @@ export async function proxy(request: NextRequest) {
   );
 
 console.log(refresh)
-  // 1. If user is already logged in, redirect away from auth pages
   if (token && isAuthRoute) {
     return NextResponse.redirect(new URL("/projects", request.url));
   }
 
-  // 2. Check if route is protected but token is missing or expired
   if (!token && isProtectedRoute) {
     
-    // 3. If we have a refresh token, attempt to generate a new access token
     if (refresh) {
       try {
-        // Call the API directly using standard fetch (Edge compatible)
         const res = await fetch(`${process.env.BASE_URL}/auth/v1/token?grant_type=refresh_token`, {
           method: "POST",
           headers: {
@@ -43,28 +39,25 @@ console.log(refresh)
         console.log(res)
         const data = await res.json(); 
         console.log(data)
-        token = data.access_token; // Retrieve the newly generated token
+        token = data.access_token; 
 
-        // 4. Create a successful response to continue the request
         const response = NextResponse.next();
 
-        // 5. Save the newly generated access token into the cookies
         response.cookies.set({
           name: accessToken,
           value: token as string,
           httpOnly: true,
           path: "/",
-          maxAge: 120, // Match your 120 seconds limit
+          maxAge: 3600, 
           sameSite: "lax",
           secure: process.env.NODE_ENV === "production",
         });
 
-        return response; // Proceed to the protected route with the new token
+        return response; 
 
       } catch (error) {
         console.error("Token refresh failed:", error);
         
-        // 6. If refresh fails, clear dead cookies and redirect to login
         const loginRedirect = NextResponse.redirect(new URL("/login", request.url));
         loginRedirect.cookies.delete(accessToken);
         loginRedirect.cookies.delete(refreshToken);
@@ -73,7 +66,6 @@ console.log(refresh)
       }
     }
 
-    // 7. No access token and no refresh token -> redirect to login
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
