e72334c4ce715bd3916e320e2ff889bb8075b0c6
hisham <heshamelsauied@gmail.com>
2026-06-07 14:36:18 +0300

    refactor(auth): convert forgotPassword api route to server actions

---
+++ app/server-actions/auth/forgotPassword.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { apiFetch } from "@/lib/api-client";
+import errorsToRecord from "@hookform/resolvers/io-ts/dist/errorsToRecord.js";
+
+type ForgotPasswordData = {
+  email: string;
+};
+
+export async function forgotPasswordAction(
+  forgotPasswordData: ForgotPasswordData,
+) {
+  try {
+    await apiFetch({
+      path: "/auth/v1/recover",
+      method: "POST",
+      body: JSON.stringify(forgotPasswordData),
+    });
+
+    return {
+      success: true,
+      message:
+        "If an account exists with this email, we've sent a password reset link.",
+    };
+  } catch (error: any) {
+    throw (
+      error || error.message || error.msg ||
+      "Internet Connection Failed or Internal Server Error"
+    );
+  }
+}
